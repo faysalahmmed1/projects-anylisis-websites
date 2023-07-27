87b8c61127ca78d943fb809f5f46e8709b2ab9bf
faysalahmmed1 <faysalahmmedcse@gmail.com>
2023-07-27 17:12:57 +0600

    forth commit

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import './Dashboard.css'
+import MyLineChart from '../MyLineChart/MyLineChart';
+import MyLineChart2 from '../MyLineChart2/MyLineChart2';
 
 const Dashboard = () => {
     return (
-        <div>
-            <h1>here is a Dashboard</h1>
+        <div className='desBoard-container'>
+            <div className='chart-1'>
+                <MyLineChart></MyLineChart>
+            </div>
+            <div className='chart-2'>
+                <MyLineChart2></MyLineChart2>
+            </div>
+
         </div>
     );
 };
---
+++ src/Components/MyLineChart2/MyLineChart2.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { Line, LineChart, XAxis, YAxis } from 'recharts';
+
+const MyLineChart2 = () => {
+    const data = [
+        {
+          name: 'Mar',
+          price: 4000,
+          pv: 2400,
+          amt: 2400,
+        },
+        {
+          name: 'Apr',
+          price: 3000,
+          pv: 1398,
+          amt: 2210,
+        },
+        {
+          name: 'May',
+          price: 2000,
+          pv: 9800,
+          amt: 2290,
+        },
+        {
+          name: 'Jun',
+          price: 2780,
+          pv: 3908,
+          amt: 2000,
+        },
+        {
+          name: 'jul',
+          price: 1890,
+          pv: 4800,
+          amt: 2181,
+        },
+        {
+          name: 'Aug',
+          price: 2390,
+          pv: 3800,
+          amt: 2500,
+        },
+        {
+          name: 'Sep',
+          price: 3490,
+          pv: 4300,
+          amt: 2100,
+        },
+      ];
+    return (
+        <LineChart width={400} height={400} data={data}>
+        <Line type="monotone" dataKey={'price'} stroke="#8884d8" />
+        <XAxis dataKey={'name'}></XAxis>
+        <YAxis dataKey={'pv'}></YAxis>
+
+        </LineChart>
+    );
+};
+
+export default MyLineChart2;
